886d27545aabe2b013aa60b5bfa0307d5189ee46
riku <starcloud.nal.aircraft@gmail.com>
2020-05-01 13:16:37 +0900

    angular frone end

---
+++ src/app/products.ts
@@ -0,0 +1,25 @@
+export const products = [
+    {
+      name: 'Phone XL',
+      price: 799,
+      coverImage: './assets/img/original.jpg',
+      description: 'A large phone with one of the best screens',
+      hedding:'I am a Pen. Because I like to so much Very Like simple. to so Happy!!!'
+    },
+    {
+      name: 'Phone Mini',
+      price: 699,
+      coverImage: './assets/img/original.jpg',
+      description: 'A great phone with one of the best cameras',
+      hedding:'I am a Pen. Because I like to so much Very Like simple. to so Happy!!!'
+
+    },
+    {
+      name: 'Phone Standard',
+      price: 299,
+      coverImage: './assets/img/original.jpg',
+      description: '',
+      hedding:'I am a Pen. Because I like to so much Very Like simple. to so Happy!!!'
+    }
+  ];
+  
