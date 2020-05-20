bb88374cd63afa31b4cc781b60192a15f52c2330
riku <starcloud.nal.aircraft@gmail.com>
2020-05-20 21:48:34 +0900

    touch

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -7,10 +7,14 @@ import { products } from '../../products';
 })
 export class ProductListComponent implements OnInit {
   products: any;
-  constructor() { }
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
-    this.products = products;
+    const productObservable = this.productService.getProducts();
+    productObservable.subscribe(
+      (deta) => {this.products = deta;},
+      (err) => {console.error('something wrong occurred: ' + err); },
+    );
   }
 
 }
